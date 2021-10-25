a74577c33ec5c151e4b8d267b67fa3610c4a528e
Sergio Noris <sergio.noris@ujed.mx>
2021-10-25 17:01:16 -0500

    Se ajusta la ruta

--- proveedores/urls.py
+++ proveedores/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import send_password
 
 urlpatterns = [
-	path('send_password/<str:email_provider>/<str:rfc_provider>/<str:pass_provider>',    send_password,  name="send-password"),
+	path('send_password',    send_password,  name="send-password"),
 ]
