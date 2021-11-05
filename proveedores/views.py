39f6d235bc9aeb2678639736680bd3419953f1e0
Sergio Noris <sergio.noris@ujed.mx>
2021-11-05 13:36:48 -0600

    Función para envío de correo de revisión de proveedores

--- proveedores/views.py
+++ proveedores/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from django.template.loader import render_to_string
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 
+import json
+
 
 # Create your views here.
 def send_email(request, data, email, subject, email_template_name):
@@ -35,3 +37,38 @@ def send_password(request):
                 'Activación de Usuario del Padrón de Proveedores UJED', 
                 'emails/provider_password.mjml')
     return HttpResponse('terminó')
+
+
+
+@api_view(['POST'])
+@authentication_classes([])
+@permission_classes([])
+def send_review(request):
+    auxiliar = request.data['tabla']
+    auxiliar = auxiliar.replace('\\\\u00e1','á')
+    auxiliar = auxiliar.replace('\\\\u00e9','é')
+    auxiliar = auxiliar.replace('\\\\u00ed','í')
+    auxiliar = auxiliar.replace('\\\\u00f3','ó')
+    auxiliar = auxiliar.replace('\\\\u00fa','ú')
+    auxiliar = auxiliar.replace('\\\\u00f1','ñ')
+    auxiliar = auxiliar.replace('?','ñ')
+    
+    tabla = json.loads(auxiliar)
+
+    for element in tabla:
+        if element['estatus_desc'] == 'ACEPTADO':
+            element.update({'color':'#D4EFDF'})
+        elif element['estatus_desc'] == 'RECHAZADO':
+            element.update({'color': '#F2D7D5'})
+        elif element['estatus_desc'] == 'EN REVISION':
+            element.update({'color': '#F2F3F4'})
+
+    send_email(request,
+                {
+                    'rfc_provider'  : request.data['rfc'],
+                    'tabla'         : tabla
+                }, 
+                request.data['email'], 
+                'Revisión del Padrón de Proveedores UJED', 
+                'emails/provider_review.mjml')
+    return HttpResponse('terminó review')
