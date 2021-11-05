3a9f6a901c04b27b600793aa42dc35d7af65d448
Sergio Noris <sergio.noris@ujed.mx>
2021-11-05 13:36:11 -0600

    Se agrega ruta para envío de revisión

--- proveedores/urls.py
+++ proveedores/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import send_password
+from .views import send_password, send_review
 
 urlpatterns = [
-	path('send_password',    send_password,  name="send-password"),
+	path('send_password',	send_password,  name="send-password"),
+	path('send_review',    	send_review,	name="send-review"),
 ]
